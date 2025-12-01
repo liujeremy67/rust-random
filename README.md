9842b5c388d9d4c9f5c8742620b16a9b09ed79e1
Jeremy <liujeremy67@gmail.com>
2025-11-30 19:17:28 -0500

    basic linked list

---
+++ README.md
@@ -0,0 +1,8 @@
+notes as i go
+
+Only one owner and one mutable reference per value.
+
+References cannot be null, requires Option in case of absence. No null pointer exceptions.
+
+Cannot store infinitely large types, indirection when referring to instance of self (nodes in linkedlist.rs).
+
---
+++ src/linkedlist.rs
@@ -0,0 +1,46 @@
+// linked list
+
+struct Node<T> {
+    val: T,
+    next: Link<T>,
+}
+
+// both to prevent types of unknown size (a linked list) and to prevent uncertainty (potentially none)
+type Link<T> = Option<Box<Node<T>>>;
+
+pub struct LinkedList<T> {
+    head: Link<T>,
+}
+
+// constructor
+// impl implements method for struct
+impl<T> LinkedList<T> {
+    pub fn new() -> Self { // -> Self returns same type as implementation
+        Self {             // create a struct literal
+            head: None,
+        }
+    }
+}
+
+// push front
+// Some part of Option (the first T enum)
+// req since we defined it as Link, which is Option
+impl<T> LinkedList<T> {
+    pub fn push(&mut self, val: T) {
+        // .take() sets head to 0, returns original value
+        let new_node = Box::new(Node { val, next: self.head.take() });
+        self.head = Some(new_node);
+    }
+}
+
+// pop front
+impl<T> LinkedList<T> {
+    pub fn pop(&mut self) -> Option<T> {
+        if let Some(prev_head) = self.head.take() { // head is wrapped in Option
+            self.head = prev_head.next; // next already wrapped, does not need Some
+            Some(prev_head.val)
+        } else {
+            None
+        }
+    }
+}
